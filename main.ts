fb8d2a47dbf254d7282ee2722a822a230d59cbee
Allan <allan.mendes0976@gmail.com>
2024-07-13 13:13:48 -0300

    .

--- main.ts
+++ main.ts
@@ -2,125 +2,64 @@ import { Logger } from './log';
 
 Logger.log('TypeScript works!');
 
-const nome = document.getElementById('nome') as HTMLInputElement;
-const sobrenome = document.getElementById('sobrenome') as HTMLInputElement;
-const email = document.getElementById('email') as HTMLInputElement;
-const button = document.getElementById('button') as HTMLButtonElement
-const form = document.getElementById('form') as HTMLFormElement;
+const SHOW_ERROR_MESSAGES = 'show-error-message';
 
-let isValidNome: boolean = false;
-let isValidEmail: boolean = false;
-let isValidSobrenome: boolean = false;
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
 
-  
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    valida(nome, sobrenome, email);
-   if (isValidNome === true && isValidSobrenome === true && isValidEmail === true) {
-     alert('Usuário cadastrado com sucesso.');
-     setTimeout(() => {
-       nome.value = '';
-       sobrenome.value = '';
-       email.value = '';
-       nome.focus();
-     }, 700);
-     form.submit();
-   } else {
-      console.log('Não foi possível, tente novamente');
-   }
+form.addEventListener('submit', function (event: Event) {
+  event.preventDefault();
+  hideErrorMessages(this);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+  checkEqualPasswords(password, password2);
+  if (shouldSendForm(this)) console.log('FORMULÀRIO ENVIADO!');
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value)
+      showErrorMessage(input, 'Este campo não pode ficar vazio.');
   });
-  
-function valida(nome: HTMLInputElement, sobrenome: HTMLInputElement, email: HTMLInputElement) {
-  validaNome(nome as HTMLInputElement);
-  validaSobrenome(sobrenome as HTMLInputElement);
-  validaEmail(email as HTMLInputElement);
 }
 
-function validaNome(nome: HTMLInputElement) {
-    console.log('Validando...');
-    let msg: string;
-    isValidNome = false;
-    if (nome.nextElementSibling && nome.nextElementSibling.classList.contains('error')) nome.nextElementSibling.remove();
-    if(nome.value === '') {
-      msg = '* Campo nome não pode ficar em branco.';
-      error(msg, nome);
-      return;
-    }
-    if(nome.value.length > 10) {
-      msg = '* Nome precisa ter menos de 10 caracteres.';
-      error(msg, nome);
-      return;
-    }
-  
-    if(nome.value.length < 3) {
-      msg = '* Nome precisa ter pelo menos de 3 caracteres.';
-      error(msg, nome);
-      return;
-    }
-  
-    if(!/^[a-zA-ZãõÂÔáéíóúÁÉÍÓÚ\s]+$/.test(nome.value)) {
-      msg = '* Caracteres especiais não são permitidos no campo nome.';
-      error(msg, nome);
-      return;
-    }
-    return isValidNome = true;
+function checkEmail(input: HTMLInputElement): void {
+  const mail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/;
+  if (!mail.test(input.value)) showErrorMessage(input, 'E-mail inválido');
+}
+
+function checkEqualPasswords(
+  password: HTMLInputElement,
+  password2: HTMLInputElement,
+) {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'Senhas não batem');
+    showErrorMessage(password2, 'Senhas não batem');
   }
-  
-  function validaSobrenome(sobrenome: HTMLInputElement) {
-  console.log('Validando sobrenome...');
-    isValidSobrenome = false;
-    let msg: string;
-    if (sobrenome.nextElementSibling && sobrenome.nextElementSibling.classList.contains('error')) sobrenome.nextElementSibling.remove();
-    if (sobrenome.value === '') {
-      msg = '* Campo sobrenome não pode ficar em branco.';
-      error(msg, sobrenome);
-     return;
-    } 
-     
-    
-      if (sobrenome.value.length > 20) {
-        let array: string[] = [];
-        for (let iterator of sobrenome.value) {
-          if(iterator === ' ') continue;
-          array.push(iterator);
-        }
-        if(array.join('').length >= 20) {
-          msg = '* sobrenome precisa ter menos de 20 caracteres.';
-          error(msg, sobrenome);
-          return;
-        }
-      }
-
-    if (!/(^[a-zA-ZãõÂÔáéíóúÁÉÍÓÚ\s]+$)|(^[a-zA-ZãõÂÔáéíóúÁÉÍÓÚ]\s[a-zA-ZãÕÂÔ]+$)/.test(sobrenome.value)) {
-      msg = '* Caracteres especiais não são permitidos no campo sobrenome.';
-      error(msg, sobrenome);
-     return;
-    }
-  return isValidSobrenome = true;
 }
 
-function validaEmail(email: HTMLInputElement){
-  let msg: string;
-  if (email.nextElementSibling && email.nextElementSibling.classList.contains('error')) email.nextElementSibling.remove();
-  if (email.value === '') {
-    msg = '* Campo E-mail precisa ser preenchido.'
-    error(msg, email);
-    return isValidEmail = false;
-  }
-  if(!/\S+@\S+\.\S+/.test(email.value)) {
-    msg = '* E-mail inválido!';
-    error(msg, email);
-    return isValidEmail = false;
-  }
-  return isValidEmail = true;
+function hideErrorMessages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
 }
 
-function error(msg: string, campo: HTMLInputElement): void {
-    const div: HTMLElement = document.createElement('div');
-    const p: HTMLElement = document.createElement('p');
-    div.classList.add('error');
-    p.textContent = msg;
-    div.appendChild(p);
-    campo.insertAdjacentElement('afterend', div);
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGES);
 }
 
+function shouldSendForm(form: HTMLFormElement): boolean {
+  let send = true;
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach(() => (send = false));
+  return send;
+}
